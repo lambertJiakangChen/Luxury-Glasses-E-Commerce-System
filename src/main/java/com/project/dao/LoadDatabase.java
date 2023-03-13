f4492dc81ca133f92c6ae6afe7c64fdf3aead0b8
lambertJiakangChen <80776373+lambertJiakangChen@users.noreply.github.com>
2023-03-13 14:51:44 -0400

    Update LoadDatabase.java

--- src/main/java/com/project/dao/LoadDatabase.java
+++ src/main/java/com/project/dao/LoadDatabase.java
@@ -19,15 +19,35 @@ public class LoadDatabase {
 	
 	  @Bean
 	  CommandLineRunner initDatabase(AccountDao accountDao, CatalogDao catalogDao) {
+            HashSet<String> color=new HashSet<String>();
+		HashSet<String> color1=new HashSet<String>();
+		HashSet<String> color2=new HashSet<String>();
+		HashSet<String> category=new HashSet<String>();
+		HashSet<String> category1=new HashSet<String>();
+		HashSet<String> category2=new HashSet<String>();
+		color.add("red");  
+		color.add("blue");
+		color.add("green");
+		color1.add("red");
+		color1.add("black");
+		color2.add("red");
+		color2.add("green");
+		color2.add("black");
+		color2.add("pink");
+		category.add("full-frame");
+		category.add("semi-rimless");
+		category.add("rimless");
+		category1.add("rimless");
+		category2.add("semi-rimless");
+		category2.add("rimless");
 
 	    return args -> {
 	    	log.info("Preloading " + accountDao.save(new Account((long) 1, "justinB31", "justin", "bieber", "justin1@gmail.com", "Qwerty12", "regular", null)));
 	    	
-
-	    	catalogDao.save(new Item((long) 1, "Evelyn", "Cat eye", "wide", 22.95, "titanium", 12.0, 56.0, 37.0, 140.0, new HashSet<String>(), new HashSet<String>()));
-	    	catalogDao.save(new Item((long) 2, "Yomary", "Cat eye", "wide", 25.95, "TR90", 19.2, 57.0, 48.0, 140.0, new HashSet<String>(), new HashSet<String>()));
-	    	catalogDao.save(new Item((long) 3, "Cicely", "Geometric", "wide", 15.00, "TR90", 20.0, 56.18, 45.96, 139.0, new HashSet<String>(), new HashSet<String>()));
-
+	    	catalogDao.save(new Item((long) 1,"Oakley" ,"Evelyn", "Cat eye", "wide", 22.95, "titanium", 12.0, 56.0, 37.0, 140.0, category, color));
+	    	catalogDao.save(new Item((long) 2,"Carrera", "Yomary", "Cat eye", "wide", 25.95, "TR90", 19.2, 57.0, 48.0, 140.0, category1, color1));
+	    	catalogDao.save(new Item((long) 3,"Oakley", "Cicely", "Geometric", "wide", 15.00, "TR90", 20.0, 56.18, 45.96, 139.0, category2, color2));
+		    
             catalogDao.findAll().forEach(item -> {
               log.info("Preloaded " + item);});
 	    };
