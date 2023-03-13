3e08819ba21bd8a02486fb76b2e3c1bbe369b828
anarraidoo <32473241+anarraidoo@users.noreply.github.com>
2023-03-13 18:03:10 -0400

    Update Item.java
    - initialize brand in Item constructor

--- src/main/java/com/project/entity/Item.java
+++ src/main/java/com/project/entity/Item.java
@@ -38,6 +38,7 @@ public class Item {
 		super();
 		this.id = id;
 		this.itemName = itemName;
+		this.brand = brand;
 		this.shape = shape;
 		this.size = size;
 		this.price = price;
@@ -245,12 +246,12 @@ public class Item {
 
 	@Override
 	public String toString() {
-		return "Item [id=" + getId() + ", brand=" + getBrand() +", itemName=" + getItemName() + ", shape=" + getShape() 
+		return "Item\n[id=" + getId() + ", brand=" + getBrand() +", itemName=" + getItemName() + ", shape=" + getShape() 
 				+ ", size=" + getSize() + ", price=" + getPrice() 
 				+ ", weight=" + getWeight() + ", material=" + getMaterial() + ", demiension=" 
 				+ getLensWidth() + "-" + getLensHeight() + "-" 
 				+ getFrameWidth() + ", category=" + getCategory()
-				+ ", colors=" + getColors() + "]";
+				+ ", colors=" + getColors() + "]\n";
 	}
 
 
