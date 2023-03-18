83026ac4c4860776754b57665bacbadd9723cb68
anarraidoo <32473241+anarraidoo@users.noreply.github.com>
2023-03-18 13:12:42 -0400

    Update LoadDatabase.java

--- src/main/java/com/project/dao/LoadDatabase.java
+++ src/main/java/com/project/dao/LoadDatabase.java
@@ -16,12 +16,12 @@ import com.project.entity.types.AccountType;
 
 @Configuration
 public class LoadDatabase {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-	
-	  @Bean
-	  CommandLineRunner initDatabase(AccountDao accountDao, CatalogDao catalogDao, SequenceDao sequenceDao) {
-        HashSet<String> color=new HashSet<String>();
+
+	@Bean
+	CommandLineRunner initDatabase(AccountDao accountDao, CatalogDao catalogDao, SequenceDao sequenceDao) {
+		HashSet<String> color=new HashSet<String>();
 		HashSet<String> color1=new HashSet<String>();
 		HashSet<String> color2=new HashSet<String>();
 		HashSet<String> color3=new HashSet<String>();
@@ -48,26 +48,27 @@ public class LoadDatabase {
 		category1.add("rimless");
 		category2.add("semi-rimless");
 		category2.add("rimless");
-		
-	
-	    return args -> {
-	    	log.info("Preloading " + sequenceDao.save(new Sequence("ORDER_ITEM", (long) 1)));
-	    	log.info("Preloading " + sequenceDao.save(new Sequence("ORDER", (long) 1)));
 
-	    	
-	    	log.info("Preloading " + accountDao.save(new Account((long) 1, "justinB31", "justin", "bieber", "justin1@gmail.com", "Qwerty12", AccountType.REGULAR, null)));
 
-	    	log.info("Preloading " + accountDao.save(new Account((long) 2, "adminAcc", "Adam", "Smith", "adam12@gmail.com", "Qwerty12", AccountType.ADMIN, null)));
+		return args -> {
+			log.info("Preloading " + sequenceDao.save(new Sequence("ORDER_ITEM", (long) 1)));
+			log.info("Preloading " + sequenceDao.save(new Sequence("ORDER", (long) 1)));
 
-	    	
-	    	catalogDao.save(new Item((long) 1, "Evelyn", "Oakley" , "Cat eye", "wide", 22.95, "titanium", 12.0, 56.0, 37.0, 140.0, category, color));
-	    	catalogDao.save(new Item((long) 2, "Yomary", "Carrera", "Cat eye", "wide", 25.95, "TR90", 19.2, 57.0, 48.0, 140.0, category1, color3));
-	    	catalogDao.save(new Item((long) 3, "Cicely", "Oakley", "Geometric", "wide", 15.00, "TR90", 20.0, 56.18, 45.96, 139.0, category2, color2));
-	        catalogDao.save(new Item((long) 4, "Yomary", "Carrera", "Cat eye", "wide", 15.00, "TR90", 20.0, 56.18, 45.96, 139.0, category, color2));
-		    
-                catalogDao.findAll().forEach(item -> {
-                    log.info("Preloaded " + item);});
-	      };
-	  }
+
+			log.info("Preloading " + accountDao.save(new Account((long) 1, "justinB31", "justin", "bieber", "justin1@gmail.com", "Qwerty12", AccountType.REGULAR, null)));
+
+			log.info("Preloading " + accountDao.save(new Account((long) 2, "adminAcc", "Adam", "Smith", "adam12@gmail.com", "Qwerty12", AccountType.ADMIN, null)));
+
+
+			catalogDao.save(new Item((long) 1, "Evelyn", "Oakley" , "Cat eye", "wide", 22.95, "titanium", 12.0, 56.0, 37.0, 140.0, category, color));
+			catalogDao.save(new Item((long) 2, "Yomary", "Carrera", "Cat eye", "wide", 25.95, "TR90", 19.2, 57.0, 48.0, 140.0, category1, color3));
+			catalogDao.save(new Item((long) 3, "Cicely", "Oakley", "Geometric", "wide", 15.00, "TR90", 20.0, 56.18, 45.96, 139.0, category2, color2));
+			catalogDao.save(new Item((long) 4, "Yomary", "Carrera", "Cat eye", "wide", 15.00, "TR90", 20.0, 56.18, 45.96, 139.0, category, color2));
+
+			catalogDao.findAll().forEach(item -> {
+				log.info("Preloaded " + item);});
+			
+		};
+	}
 
 }
