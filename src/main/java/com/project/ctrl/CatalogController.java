416a1a9cdf5c9789342d9c40e6e9d9f96d608532
zain852 <126923029+zain852@users.noreply.github.com>
2023-03-19 21:48:32 -0400

    Update CatalogController.java

--- src/main/java/com/project/ctrl/CatalogController.java
+++ src/main/java/com/project/ctrl/CatalogController.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.catalog.CatalogService;
 import com.project.dao.CatalogDao;
